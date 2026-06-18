5b5fb007ebccba9217d985c70b9824b9ec7a7c8c
mroiviliabdou9-art <mroiviliabdou9@gmail.com>
2026-06-18 10:20:48 +0300

    Add files via upload
    Mise à jour app.py

--- app.py
+++ app.py
@@ -4,38 +4,58 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = 'projet_2k26_secret'
-
-# Chemin absolu pour la base de données (indispensable pour Render)
 DB_PATH = os.path.join(os.path.dirname(__file__), 'universite.db')
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    # On supprime les anciennes tables pour éviter les conflits
+    cursor.execute('DROP TABLE IF EXISTS etudiants')
+    cursor.execute('DROP TABLE IF EXISTS notes')
+    cursor.execute('DROP TABLE IF EXISTS admins')
+    
+    # On recrée tout proprement
+    cursor.execute('''CREATE TABLE etudiants (id TEXT PRIMARY KEY, nom TEXT, mention TEXT, mdp TEXT)''')
+    cursor.execute('''CREATE TABLE notes (id TEXT, matiere TEXT, note REAL, annee TEXT)''')
+    cursor.execute('''CREATE TABLE admins (id TEXT PRIMARY KEY, password TEXT)''')
+    
+    # Insertion des 3 admins
+    admins = [('admin1', '123'), ('admin2', '123'), ('admin3', '123')]
+    cursor.executemany('INSERT INTO admins VALUES (?, ?)', admins)
+    
+    conn.commit()
+    conn.close()
+
+init_db()
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
-# --- CONNEXION UNIQUE ---
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         identifiant = request.form.get('identifiant')
         password = request.form.get('password')
         
+        conn = get_db()
         # Test Admin
-        if identifiant == 'admin' and password == 'admin':
+        admin = conn.execute('SELECT * FROM admins WHERE id=? AND password=?', (identifiant, password)).fetchone()
+        if admin:
             session['role'] = 'admin'
             return redirect(url_for('admin_panel'))
         
         # Test Étudiant
-        conn = get_db()
         etu = conn.execute('SELECT * FROM etudiants WHERE id=? AND mdp=?', (identifiant, password)).fetchone()
         conn.close()
-        
         if etu:
             session['role'] = 'etudiant'
             session['user_id'] = identifiant
             return redirect(url_for('releve_notes'))
             
     return render_template('index.html')
+
 
 @app.route('/admin')
 def admin_panel():
